0536f3bb69a86d962b62b94324ab67959c365af0
Priyanshu Singh <priyanshusingh302@gmail.com>
2023-12-25 00:49:48 +0530

    History Page

--- frontend/src/helpers/axios_helper.js
+++ frontend/src/helpers/axios_helper.js
@@ -15,17 +15,25 @@ export const removeAuthToken = () => {
 axios.defaults.baseURL = 'http://localhost:8080';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-export const request = (method, url, data) => {
+export const request = async (method, url, data) => {
 
     let headers = {};
     if (getAuthToken() !== null && getAuthToken() !== "null") {
         headers = { 'Authorization': `Bearer ${getAuthToken()}` };
     }
 
-    return axios({
-        method: method,
-        url: url,
-        headers: headers,
-        data: data
-    });
+    try {
+        const response = await axios({
+            method: method,
+            url: url,
+            headers: headers,
+            data: data
+        });
+        return { success: true, data: response.data };
+    }
+    catch (err) {
+        console.log(err);
+        return { success: false, data: {} };
+
+    }
 };
--- frontend/src/pages/HistoryPage.js
+++ frontend/src/pages/HistoryPage.js
@@ -1,9 +1,76 @@
+import { Box, List, ListItem, Paper, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 
+var numberList = [1, 2, 3, 4, 5];
+
+const MyCard = ({ item }) => {
+
+    return (
+        <>
+            <Paper elevation={2}>
+                <div style={{ width: 460, height: 80 }}>
+                    {item}
+                </div>
+            </Paper>
+        </>
+    )
+};
 
 const HistoryPage = () => {
 
-    return (
-        <h1>history</h1>
+    return (<>
+        <Container sx={{ minHeight: "90vh", mt: 1, display: "flex", flexDirection: "column", alignContent: "center", alignItems: "center" }}>
+            <Box width={1000} mb={1} mt={1}>
+                <Typography
+                    fontSize={36}
+                >
+                    Histroy
+                </Typography>
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+                <Box border={1} borderColor="#dddddd" height="70vh" width={500} mr={5} boxShadow={2} borderRadius="10px">
+                    <Typography
+                        bgcolor="#d1d1d1"
+                        borderRadius="10px 10px 0 0"
+                        align="center"
+                        fontSize={26}
+                        fontWeight="bold"
+                        fontFamily='monospace'
+                    >
+                        Gave Away
+                    </Typography>
+                    <List style={{ maxHeight: '90%', overflow: 'auto' }} >
+                        {numberList.map(item =>
+                        (
+                            <ListItem>
+                                <MyCard item={item} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                <Box border={1} borderColor="#dddddd" height="70vh" width={500} boxShadow={2} borderRadius="10px">
+                    <Typography
+                        bgcolor="#d1d1d1"
+                        borderRadius="10px 10px 0 0"
+                        align="center"
+                        fontSize={26}
+                        fontWeight="bold"
+                        fontFamily='monospace'
+                    >
+                        Taken
+                    </Typography>
+                    <List style={{ maxHeight: '90%', overflow: 'auto' }}>
+                        {numberList.map(item =>
+                        (
+                            <ListItem>
+                                <MyCard item={item} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Box>
+        </Container>
+    </>
     )
 }
 
